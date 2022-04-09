f297a754bc14ecaa475f41a1a0b5762491fd3376
kema-ray <rachelkemuma99@gmail.com>
2022-04-09 17:50:51 +0300

    add information to the blueprint class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,11 @@
 export class Quotes {
+    showDescription: boolean;
+    constructor(
+        public id: number,
+        public author: string,
+        public quote: string,
+        public publishedDate: Date
+    ){
+        this.showDescription=false;
+    }
 }
