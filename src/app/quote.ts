0fdd6724dbf65afd9cb8fc0c14a02de93882c9c6
kema-ray <rachelkemuma99@gmail.com>
2022-04-09 18:19:52 +0300

    add two-way data binding

---
+++ src/app/quote.ts
@@ -0,0 +1,10 @@
+export class Quote {
+    showQuote: boolean;
+    constructor(
+        public id: number,
+        public author: string,
+        public quote: string
+    ){
+        this.showQuote=false;
+    }
+}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote(1, 'Lupita Nyongo', 'Wakanda Forever'),
+    new Quote(2, 'Lupita Nyongo', 'Wakanda Forever'),
+    new Quote(3, 'Lupita Nyongo', 'Wakanda Forever')
+  ]
 
   constructor() { }
 
