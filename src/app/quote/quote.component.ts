d12934ebe209353dbfae19726a8a3aae307abf46
kema-ray <rachelkemuma99@gmail.com>
2022-04-10 00:04:40 +0300

    use ngSubmit to post quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -27,6 +27,11 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    this.quotes.push(quote)
+  }
 
   constructor() { }
 
